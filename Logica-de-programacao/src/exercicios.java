dba346f709c5cb5b114aa92fe05f5d3ed65c946e
dillfaustino <dilfaustino87@gmail.com>
2024-05-11 16:21:44 -0300

    Add files via upload

---
+++ Logica-de-programacao/src/exercicios.java
@@ -0,0 +1,44 @@
+public class exercicios {
+    String gameRpg = "Hogwarts Legacy";
+    String gameIndie = "Hogawarts Knigt";
+
+    int age = 35;
+
+    int code = 2367;
+
+    char gender = 'M';
+
+    double priceRpg = 107.80;
+
+    double princeIndie = 46.99;
+
+    public static void main(String[]args){
+
+        string gameRpg, gameIndie;
+
+        int age, code;
+        char gender;
+        double priceRpg = 107.80;
+
+        gameIndie = "Hollow Knight";
+
+        age =35;
+        code = 2367;
+
+        media = (priceRpg + priceindie) /2;
+
+        System.out.println("Produtos:");
+        System.out.println(gameRpg + ",esta custando R$" + priceRpg);
+        System.out.println(gameIndie + ", esta custando R$ + priceIndie");
+        System.out.printf("O comprador tinha %d anos, codigo %d e genero: %s" ,age, code, gender);
+        System.out.printf("A Media de preço dos produtos é igual a %f" , media);
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Logica-de-programacao/src/tipos/primitivos/Inicio.java
@@ -0,0 +1,24 @@
+package tipos.primitivos;
+
+public class Inicio {
+
+    public static void main(String[] args) {
+
+        int idadeUsuario = 21;
+        String nomeUsuario = "Adilson";
+        float alturaUsuario = 1.73F;
+
+
+
+
+        System.out.println("Ola Mundo");
+        System.out.println("Nome:"+ nomeUsuario);
+        System.out.println("Altura: "+ alturaUsuario);
+
+    }
+}
+
+
+
+
+
